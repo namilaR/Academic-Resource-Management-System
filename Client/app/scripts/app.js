14203b6c3f133494c76f4e8c4619d348e63120bf
namilaR <n.radith@gmail.com>
2016-12-14 17:13:47 +0530

    Final Merge conflicts resolve

--- Client/app/scripts/app.js
+++ Client/app/scripts/app.js
@@ -8,24 +8,21 @@
  * Main module of the application.
  */
 
-
 angular
   .module('armsAngularApp', [
     'ngAnimate',
     'ngCookies',
     'ngResource',
-    'ui.bootstrap',
     'ngRoute',
     'ngSanitize',
     'ngTouch',
+    'ui.bootstrap',
     'datatables',
     'datatables.bootstrap',
     'ui.select2',
-    'mgcrea.ngStrap',
-    'checklist-model',
     'toggle-switch'
   ])
-  .config(['$routeProvider', '$locationProvider', '$httpProvider', function ($routeProvider, $locationProvider) {
+  .config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
     $routeProvider
 
     /*******************************/
@@ -50,26 +47,29 @@ angular
       /*******************************/
 
       /*  main page route  */
-      .when('/', {
-        /*  non admin users  */
+      .when('/', {             /*  non admin users  */
         templateUrl: 'views/user_main.html',
         controller: 'MainCtrl',
         controllerAs: 'main'
       })
-      .when('/control-panel', {
-        /*  admin user   */
+      .when('/control-panel', { /*  admin user   */
         templateUrl: 'views/admin_main.html',
         controller: 'MainCtrl',
         controllerAs: 'main'
       })
 
-
       /*  admin - backend management  routes  */
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
       .when('/control-panel/faculty', {
         templateUrl: 'views/faculty/main.html',
         controller: 'FacultyMainCtrl',
         controllerAs: 'facultyController'
       })
+
       /*  user - management  routes  */
       .when('/control-panel/users', {
         templateUrl: 'views/user_management/users.html',
@@ -81,50 +81,32 @@ angular
         controller: 'UserTypeCtrl',
         controllerAs: 'usertype'
       })
-      .when('/about', {
-        templateUrl: 'views/about.html',
-        controller: 'AboutCtrl',
-        controllerAs: 'about'
+
+      /*  question and question template - management  routes  */
+      .when('/control-panel/questions', {
+        templateUrl: 'views/questions/questions.html',
+        controller: 'QuestionsCtrl',
+        controllerAs: 'questions'
       })
+      .when('/control-panel/question-templates', {
+        templateUrl: 'views/question_template/questionTemplate.html',
+        controller: 'QuestionTemplateCtrl',
+        controllerAs: 'questionTemplate'
+      })
+
+
+
+
       /*  appointment - management  routes  */
-      .when('/hod/appointments/appointment', {
-        templateUrl: 'views/appointments/hod-appoinments.html',
-        controller: 'HodAppointmentsCtrl',
-        // controllerAs: 'appointment',
-        // bindToController: 'true'
-      })
-      .when('/l/appointments/appointment', {
+      .when('/appointments/appointment', {
         templateUrl: 'views/appointments/appointment.html',
         controller: 'AppointmentCtrl',
         controllerAs: 'appointment',
         bindToController: 'true'
       })
-      .when('/s/appointments/appointment', {
-        templateUrl: 'views/appointments/appointment-request.html',
-        controller: 'AppointmentRequestsCtrl',
-        controllerAs: 'appointmentRequest',
-        bindToController: 'true'
 
-      })
-      .when('/l/appointments/my-availability', {
-        templateUrl: 'views/appointments/lecture-avilability.html',
-        controller: 'LectureAvilabilityCtrl',
-        controllerAs: 'lectureAvailability',
-        bindToController: 'true'
 
-      })
 
-      /*  question and question template - management  routes  */
-      .when('/control-panel/questions', {
-          templateUrl: 'views/questions/questions.html',
-          controller: 'QuestionsCtrl',
-          controllerAs: 'questions'
-      })
-      .when('/control-panel/question-templates', {
-          templateUrl: 'views/question_template/questionTemplate.html',
-          controller: 'QuestionTemplateCtrl',
-          controllerAs: 'questionTemplate'
-      })
       /*  feedback - management  routes  */
       .when('/feedback-management/feedback-sessions', {
         templateUrl: 'views/feedback_management/feedback_sessions.html',
@@ -133,69 +115,39 @@ angular
         bindToController: 'true'
       })
 
-      .when('/admin/questions', {
-        templateUrl: 'views/questions/questions.html',
-        controller: 'QuestionsCtrl',
-        controllerAs: 'questions'
-      })
-      .when('/hod/feedbackSession', {
-        templateUrl: 'views/feedbackSession/feedbackSession.html',
-        controller: 'QuestionsCtrl',
-        controllerAs: 'questions'
-      })      
       .when('/feedback-management/report', {
-          templateUrl: 'views/reports/chart.html',
-          controller: 'ReportCtrl',
-          controllerAs: 'report',
-          bindToController: 'true'
+        templateUrl: 'views/reports/chart.html',
+        controller: 'ReportCtrl',
+        controllerAs: 'report',
+        bindToController: 'true'
       })
 
       /*  quiz routes  */
       .when('/feedback/quiz-main', {
-          templateUrl: 'views/quiz/quiz_main.html',
-          controller: 'QuizCtrl',
-          controllerAs: 'QuizCtrl',
-          bindToController: 'true'
+        templateUrl: 'views/quiz/quiz_main.html',
+        controller: 'QuizCtrl',
+        controllerAs: 'QuizCtrl',
+        bindToController: 'true'
       })
-      .when('/feedback/quiz', {
-          templateUrl: 'views/quiz/quiz.html',
-          controller: 'QuizCtrl',
-          controllerAs: 'QuizCtrl',
-          bindToController: 'true'
+      .when('/feedback/quiz/:sessionCode?', {
+        templateUrl: 'views/quiz/quiz.html',
+        controller: 'QuizCtrl',
+        controllerAs: 'QuizCtrl',
+        bindToController: 'true'
       })
-      /*  faculty - management  routes  */
-      .when('/control-panel/faculty/main', {
-        templateUrl: 'views/faculty/main.html',
-        controller: 'FacultyMainCtrl',
-        controllerAs: 'facultyController'
-      })
-      .when('/control-panel/admin/subjects', {
-        templateUrl: 'views/subjects/subjects.html',
-        controller: 'SubjectsCtrl',
-        controllerAs: 'subjects'
-      })
-      .when('/control-panel/admin/center', {
-        templateUrl: 'views/center/center.html',
-        controller: 'CenterCenterCtrl',
-        controllerAs: 'Center/Center'
-      })
-      .when('/control-panel/admin/departments', {
-        templateUrl: 'views/department/department.html',
-        controller: 'DepartmentCtrl',
-        controllerAs: 'Department'
-      })
-      .when('/control-panel/admin/batch', {
-        templateUrl: 'views/batch/batch.html',
-        controller: 'BatchCtrl',
-        controllerAs: 'Batch'
-      })
+
+
+
+
+
+
+
+
       .otherwise({
         redirectTo: '/'
       });
-      $locationProvider.html5Mode(false);
-
-  }]).run(function ($rootScope, $location, AuthenticationService, AUTH_EVENTS) {
-  $rootScope.$on('$routeChangeSuccess', function (event, currentRoute, previous) {
+  }]).run(function ($rootScope,$location, AuthenticationService,AUTH_EVENTS) {
+  $rootScope.$on('$routeChangeSuccess', function (event,currentRoute, previous) {
 
     /*******************************/
     /*       non authenticated     */
@@ -205,11 +157,10 @@ angular
     if (!AuthenticationService.isAuthenticated()) {
       event.preventDefault();
       $rootScope.bodyClass = 'login-page';
-      $location.path("/login");
-    } else {
+      $location.path( "/login" );
+    }else{
       // get logged in user`s user role
       var user_credentials = AuthenticationService.getUserCredentials();
-      $rootScope.user = user_credentials;
       var user_role = user_credentials.usertype;
       $rootScope.user_role = user_credentials.usertype;
       $rootScope.user_name = user_credentials.username;
@@ -220,24 +171,27 @@ angular
       var current_requested_path = $location.path();
       var check_path = current_requested_path.substring(14, 1);
 
-      if (user_role == 'Admin') {/*  admin routes check  */
-        $rootScope.bodyClass = 'hold-transition skin-blue sidebar-mini';
-        /*  style for admin control panel  */
-        if (check_path != 'control-panel' || $location.path() == "/login") {
-          $location.path("/control-panel");
+      if( user_role == 'Admin'){/*  admin routes check  */
+        $rootScope.bodyClass = 'hold-transition skin-blue sidebar-mini'; /*  style for admin control panel  */
+        if( check_path != 'control-panel' || $location.path() ==  "/login"){
+          $location.path( "/control-panel" );
         }
       }
       else {                    /*  non admin routes check  */
-        $rootScope.bodyClass = 'hold-transition skin-blue sidebar-mini layout-top-nav';
-        /*  style for non admin  */
-        if (check_path == 'control-panel' || $location.path() == "/login") {
+        $rootScope.bodyClass = 'hold-transition skin-blue sidebar-mini layout-top-nav'; /*  style for non admin  */
+        if( check_path == 'control-panel' || $location.path() ==  "/login") {
           $location.path("/");
         }
       }
     }
 
   });
-}).constant("moment", moment);
 
-
- 
+}).constant('CONFIG', {
+  'APP_NAME': 'Acadamic Resource Management',
+  'APP_VERSION': '0.0.1',
+  'GOOGLE_ANALYTICS_ID': '',
+  'BASE_URL': 'http://localhost:8002/',
+  'SYSTEM_LANGUAGE': ''
+})
+  .constant("moment", moment);
